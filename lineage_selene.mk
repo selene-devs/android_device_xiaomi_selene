13eb060379b441a0c2352459ac7e1f1568288279
raghavt20 <raghavtandon2000@gmail.com>
2021-12-07 21:21:47 +0530

    selene: Set TARGET_BOOT_ANIMATION_RES := 1080

--- lineage_selene.mk
+++ lineage_selene.mk
@@ -37,6 +37,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 # Some specific flags
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 ## Device identifier. This must come after all inclusions
