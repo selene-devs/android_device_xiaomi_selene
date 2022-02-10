05d8d267687786e13adfb905b22601cc351fa30e
Mikazuuu07 <irfn4566@gmail.com>
2022-02-10 10:02:36 +0000

    selene: Don't build snap camera from source
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- device.mk
+++ device.mk
@@ -124,7 +124,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4.vendor \
     android.hardware.camera.provider@2.5 \
