fd4244cc52636ecf87b326d312d22e6f33fa4023
bengris32 <bengris32@protonmail.ch>
2022-07-20 23:37:58 +0530

    selene: Introduce Mediatek In-Call Service

--- device.mk
+++ device.mk
@@ -66,6 +66,9 @@ PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
     libtinycompress.vendor
 
+PRODUCT_PACKAGES += \
+    MtkInCallService
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(DEVICE_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
