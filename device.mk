d06cf4bc31427874178a882f76c9927e195cfbb6
bengris32 <bengris32@protonmail.ch>
2022-10-04 20:47:49 +0100

    selene: Build dummy libmtkperf_client library
    * Required for some vendor blobs that refuse to
      work without mtkpower
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -301,6 +301,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.example
 
 PRODUCT_PACKAGES += \
+    libmtkperf_client_vendor \
+    libmtkperf_client
+
+PRODUCT_PACKAGES += \
     android.hardware.power@1.0 \
     android.hardware.power@1.0.vendor \
     android.hardware.power@1.1 \
