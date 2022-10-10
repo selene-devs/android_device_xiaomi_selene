23a6102e28790894df42dac380022fd57df668f7
Michael Bestas <mkbestas@lineageos.org>
2022-10-10 22:37:47 +0300

    selene: Opt out of EPPE

--- lineage_selene.mk
+++ lineage_selene.mk
@@ -27,6 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 #
