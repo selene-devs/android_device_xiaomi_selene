34303ab4540553fee6292e090155441ba6044725
Jabiyeff <cebiyevanar@gmail.com>
2022-10-16 18:34:14 +0400

    selene: parts: Import XiaomiParts for refresh rate control

--- device.mk
+++ device.mk
@@ -247,6 +247,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libpcap.vendor
     
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
