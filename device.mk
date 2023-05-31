732837bc7d4cea4d6513b2e8baacc59d870c5689
Tim Zimmermann <tim@linux4.de>
2023-05-31 17:32:56 +0200

    selene: Switch to new AIDL memtrack HAL

--- device.mk
+++ device.mk
@@ -161,10 +161,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     libdrm.vendor \
     libvulkan
+
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack-service.mediatek-mali
 
 # DRM
 PRODUCT_PACKAGES += \
