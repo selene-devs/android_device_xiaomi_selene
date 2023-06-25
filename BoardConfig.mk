0672d73e5d8c6f50ab84aff4905759890cd40fdc
atharvnegi <atharvnegi2951@gmail.com>
2023-06-25 14:04:08 +0530

    selene: Drop PowerHal Extension
    not needed as we are using libperfmgr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,7 @@ BOARD_HAS_MTK_HARDWARE := true
 BOARD_HAVE_MTK_FM := true
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
